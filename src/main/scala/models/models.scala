dabd0ced0788b577f793f094483f5b2fdc3d5d0a
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-01-19 14:19:33 +0100

    Refactoring

--- src/main/scala/models/models.scala
+++ src/main/scala/models/models.scala
@@ -1,6 +1,8 @@
 package models
 
 import akka.http.scaladsl.model.{HttpProtocol, HttpProtocols}
+import io.circe.Decoder
+import io.circe.generic.semiauto._
 
 case class Target(scheme: String, host: String, port: Int, weight: Int = 1, protocol: HttpProtocol = HttpProtocols.`HTTP/1.1`) {
   def url: String = s"$scheme://$host:$port"
@@ -25,4 +27,10 @@ object Target {
   def protoWeight(url: String, weight: Int, protocol: HttpProtocol): Target = {
     Target(url).copy(weight = weight, protocol = protocol)
   }
+}
+
+case class Command(action: String, domain: String, target: String)
+
+object Command {
+  val decoder: Decoder[Command] = deriveDecoder[Command]
 }
--- src/main/scala/util/util.scala
+++ src/main/scala/util/util.scala
@@ -6,6 +6,9 @@ import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 import akka.http.scaladsl.{ConnectionContext, HttpsConnectionContext}
 
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
+
 object HttpsSupport {
   def context(): HttpsConnectionContext = {
     val password: Array[Char] = "uW8WlTlANX0WxAo1PvDnQBGUXB1UeQrVvitD22yLiJxkxtJLz3gFzcVoKu25GJLW".toCharArray
@@ -25,5 +28,31 @@ object HttpsSupport {
     val sslContext: SSLContext = SSLContext.getInstance("TLS")
     sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
     ConnectionContext.https(sslContext)
+  }
+}
+
+object Retry {
+
+  private[this] def retryPromise[T](times: Int, promise: Promise[T], failure: Option[Throwable],
+                                    f: => Future[T])(implicit ec: ExecutionContext): Unit = {
+    (times, failure) match {
+      case (0, Some(e)) =>
+        promise.tryFailure(e)
+      case (0, None) =>
+        promise.tryFailure(new RuntimeException("Failure, but lost track of exception :-("))
+      case (_, _) =>
+        f.onComplete {
+          case Success(t) =>
+            promise.trySuccess(t)
+          case Failure(e) =>
+            retryPromise[T](times - 1, promise, Some(e), f)
+        }(ec)
+    }
+  }
+
+  def retry[T](times: Int)(f: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
+    val promise = Promise[T]()
+    retryPromise[T](times, promise, None, f)
+    promise.future
   }
 }
